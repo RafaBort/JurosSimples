9ce700b89ca72e3dbee9e5abf50bea352d2483ff
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-05-24 21:46:42 -0300

    exercicío resolvido

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o capital...");
+            string capital = Console.ReadLine();
+
+            Console.WriteLine("Digite taxa dos juros...");
+            string taxa = Console.ReadLine();
+
+            Console.WriteLine("Digite o tempo...");
+            string tempo = Console.ReadLine();
+
+            Double capital1 = Convert.ToDouble(capital);
+            Double taxa1 = Convert.ToDouble(taxa);
+            Double tempo1 = Convert.ToDouble(tempo);
+
+            Double juros = capital1 * taxa1 * tempo1;
+            Double montante = capital1 + juros;
+
+            Console.WriteLine("juros:... " + juros);
+            Console.WriteLine("montante: " + montante);
+
+        }
+    }
+}
